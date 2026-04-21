2aa9aad7348a2c8344f7ffcb42c5d5fe6b7dfa33
Chung8-J <chungjingjia137@gmail.com>
2026-04-21 18:25:33 +0800

    add __init__.py to all file

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.routes import router
+from app.api.routes import router  # ← change "api.routes" to "app.api.routes"
 
-app = FastAPI();
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # later restrict to frontend URL
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 app.include_router(router)
